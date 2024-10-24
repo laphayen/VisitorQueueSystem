cbdd63a33098277c7d224148d811c7c4647d9292
Baek Gi Chan <laphayen@gmail.com>
2024-10-24 23:43:59 +0900

    #1 - Initialize the target web project
    * Add Spring Web dependency
    * Add Thymeleaf dependency

---
+++ VisitorQueueSystemWebsite/src/test/java/com/laphayen/VisitorQueueSystemWebsite/VisitorQueueSystemWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laphayen.VisitorQueueSystemWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorQueueSystemWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
