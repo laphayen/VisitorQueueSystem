6c30709cb32c8694a94ecf7cb4d56e6396d574f5
Baek Gi Chan <laphayen@gmail.com>
2024-10-26 00:06:48 +0900

    #3 - Initialize the user queue system project
    - Added `spring-boot-starter-data-redis-reactive` for reactive Redis operations
    - Added `spring-boot-starter-thymeleaf` for server-side rendering with Thymeleaf
    - Added `spring-boot-starter-validation` for input validation support
    - Included `spring-boot-starter-webflux` for building reactive web applications
    - Added Lombok for reducing boilerplate code and configured with `compileOnly` and `annotationProcessor`
    - Added `spring-boot-devtools` for development convenience

---
+++ VisitorQueueSystemFlow/src/main/java/com/laphayen/VisitorQueueSystemFlow/VisitorQueueSystemFlowApplication.java
@@ -0,0 +1,13 @@
+package com.laphayen.VisitorQueueSystemFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorQueueSystemFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitorQueueSystemFlowApplication.class, args);
+	}
+
+}
---
+++ VisitorQueueSystemFlow/src/test/java/com/laphayen/VisitorQueueSystemFlow/VisitorQueueSystemFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laphayen.VisitorQueueSystemFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorQueueSystemFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
