0a21a97d88f0a3a649f0f2802a6dc4221e81fcb3
Baek Gi Chan <laphayen@gmail.com>
2024-12-28 23:55:19 +0900

    #1 - Add root mapping to VisitorQueueSystemWebsiteApplication
    Added a `@GetMapping("/")` method to handle root URL requests.
    Configured the method to return "index", enabling the index.html page to serve as the default landing page.

--- VisitorQueueSystemWebsite/src/main/java/com/laphayen/VisitorQueueSystemWebsite/VisitorQueueSystemWebsiteApplication.java
+++ VisitorQueueSystemWebsite/src/main/java/com/laphayen/VisitorQueueSystemWebsite/VisitorQueueSystemWebsiteApplication.java
@@ -2,12 +2,19 @@ package com.laphayen.VisitorQueueSystemWebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class VisitorQueueSystemWebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VisitorQueueSystemWebsiteApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+
 	}
 
 }
