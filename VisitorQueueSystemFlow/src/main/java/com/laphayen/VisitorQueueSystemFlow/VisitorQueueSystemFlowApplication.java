0b2c306bae11e813825d3a26839a257af5e359bc
Baek Gi Chan <laphayen@gmail.com>
2025-01-02 23:50:34 +0900

    #5 - Remove Redis initialization from VisitorQueueSystemFlowApplication

--- VisitorQueueSystemFlow/src/main/java/com/laphayen/VisitorQueueSystemFlow/VisitorQueueSystemFlowApplication.java
+++ VisitorQueueSystemFlow/src/main/java/com/laphayen/VisitorQueueSystemFlow/VisitorQueueSystemFlowApplication.java
@@ -1,25 +1,13 @@
 package com.laphayen.VisitorQueueSystemFlow;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
 
 @SpringBootApplication
-@RequiredArgsConstructor
-public class VisitorQueueSystemFlowApplication implements ApplicationListener<ApplicationReadyEvent> {
-
-	private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
+public class VisitorQueueSystemFlowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VisitorQueueSystemFlowApplication.class, args);
-	}
-
-	@Override
-	public void onApplicationEvent(ApplicationReadyEvent event) {
-		reactiveRedisTemplate.opsForValue().set("testKey", "testValue").subscribe();
 	}
 
 }
